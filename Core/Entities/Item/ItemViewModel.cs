b4baaa93b809e425d168debc60071548bb1f9ad5
tapendra jaishi <tapendra.jaishi2058@gmail.com>
2025-10-30 19:29:58 +0000

    add item section

---
+++ Core/Entities/Item/ItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+namespace Core
+{
+    public class ItemViewModel
+    {
+    }
+    public class AddItemViewModel
+    {
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public long ItemTypeId { get; set; }
+        public List<IFormFile> Images {get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
+        public string? Status { get; set; }
+    }
+}
---
+++ Trash/Controllers/ItemController.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trash
+{
+    [ApiController]
+    [Route("apiv1/[controller]/[action]")]
+    public class ItemController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddItem([FromForm]AddItemViewModel model)
+        {
+            return Ok();
+        }
+    }
+}
